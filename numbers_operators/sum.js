7c434c0b2787a6103c4161f734ed2e47a22cef2f
Viviane Martini <martini.viviane@gmail.com>
2023-05-24 11:26:48 -0300

    sum

---
+++ numbers_operators/sum.js
@@ -0,0 +1,24 @@
+/* 
+  Solicite 2 números, faça a soma deles
+  e apresente o resultado final ao usuário
+*/
+
+alert("Iremos somar 2 números")
+let numberOne = prompt("Digite o primeiro número:")
+let numberTwo = prompt("Digite o segundo número:")
+
+//para transformar em numerio - colocar dentro de uma função - função construtora
+let result = Number(numberOne) + Number(numberTwo)
+alert("Resultado final: " + result)
+
+
+/*
+
+alert('2' + 2)
+string + number = ele concatena 
+
+palavra reservada não pode ser usada como variavel
+
+typeof - responde o tipo do dado
+
+*/
