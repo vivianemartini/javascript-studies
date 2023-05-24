4f773dc83c5d48c5f17b634910dfb5934f428f30
Viviane Martini <martini.viviane@gmail.com>
2023-05-24 12:48:13 -0300

    operators

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Rocketseat Classes
 
-1 - Intro to logic
-2 - Numbers and operators
+1 - Intro to logic <br>
+2 - Numbers and operators <br>
---
+++ numbers_operators/operators.js
@@ -0,0 +1,36 @@
+/*
+
+#melhorar a  lógica de programção
+
+- fazer as perguntas corretas
+- entender o problema
+
+  Capturar 2 números
+  e fazer as operações matemáticas
+  de soma, subtração, multiplicação,
+  divisão e resto da divisão.
+
+  E para cada operação, mostrar um alerta
+  com o resultado.
+
+  let eu posso mudar
+  const não se muda
+*/
+
+let firstNumber = prompt('Digite o primeiro número')
+let secondNumber = prompt('Digite o segundo número')
+
+firstNumber = Number(firstNumber)
+secondNumber = Number(secondNumber)
+
+const sum = firstNumber + secondNumber
+const sub = firstNumber - secondNumber
+const multi = firstNumber * secondNumber
+const div = firstNumber / secondNumber
+const restDiv = firstNumber % secondNumber
+
+alert('Soma: ' + sum)
+alert('Subtração: ' + sub)
+alert('Multiplicação: ' + multi)
+alert('Divisão: ' + div)
+alert('Resto da divisão: ' + restDiv)
