58fcfe3dae625fb5a27de88fac2d7375393deaea
Viviane Martini <martini.viviane@gmail.com>
2023-06-06 10:37:13 -0300

    add object literals

--- stage5/projeto-imc/main.js
+++ stage5/projeto-imc/main.js
@@ -12,9 +12,28 @@ form.onsubmit = handleSubmit
 
 function handleSubmit(){} */
 
-const modalWrapper = document.querySelector(".modal-wrapper");
-const modalMessage = document.querySelector(".modal .title span");
-const modalBtnClose = document.querySelector(".modal button.close");
+/* const modalWrapper = ;
+const modalMessage = ;
+const modalBtnClose = ;
+
+for criado com const mas mudou para Modal com o objeto literal
+
+*/
+
+//object literals
+const Modal = {
+
+    wrapper: document.querySelector(".modal-wrapper"),
+    message: document.querySelector(".modal .title span"),
+    buttonClose: document.querySelector(".modal button.close"),
+
+    open () {
+        Modal.wrapper.classList.add("open")
+    },
+    close () {
+        Modal.wrapper.classList.remove("open")
+    }
+}
 
 form.onsubmit = (event) => {
   event.preventDefault();
@@ -25,11 +44,11 @@ form.onsubmit = (event) => {
   const result = IMC(weight, height);
   const message = `Seu IMC é de ${result}`;
 
-  modalMessage.innerText = message;
-  modalWrapper.classList.add("open");
+  Modal.message.innerText = message;
+  Modal.open()
 };
 
-modalBtnClose.onclick = () => modalWrapper.classList.remove("open");
+Modal.buttonClose.onclick = () => Modal.close();
 
 function IMC(weight, height) {
   return (weight / (height / 100) ** 2).toFixed(2);
