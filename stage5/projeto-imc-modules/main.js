a69206750f80a2f99b852404718e4f2a13ce0777
Viviane Martini <martini.viviane@gmail.com>
2023-06-06 13:50:46 -0300

    add projeto imc com modules

---
+++ stage5/projeto-imc-modules/main.js
@@ -0,0 +1,22 @@
+import { Modal } from './modal';
+
+const form = document.querySelector("form");
+const inputWeight = document.querySelector("#weight");
+const inputHeight = document.querySelector("#height");
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const weight = inputWeight.value;
+  const height = inputHeight.value;
+
+  const result = IMC(weight, height);
+  const message = `Seu IMC é de ${result}`;
+
+  Modal.message.innerText = message;
+  Modal.open()
+};
+
+function IMC(weight, height) {
+  return (weight / (height / 100) ** 2).toFixed(2);
+}
