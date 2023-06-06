bdceb9ba91ce264039ea39715489e9431eeefa98
Viviane Martini <martini.viviane@gmail.com>
2023-06-06 10:25:35 -0300

    add onsubmit fucntion and modal script

--- stage5/projeto-imc/main.js
+++ stage5/projeto-imc/main.js
@@ -1,8 +1,7 @@
 //variables
-const form = document.querySelector('form')
-const inputWeight = document.querySelector('#weight')
-const inputHeight = document.querySelector('#height')
-
+const form = document.querySelector("form");
+const inputWeight = document.querySelector("#weight");
+const inputHeight = document.querySelector("#height");
 
 //3 maneiras de criar e atribuir função a um evento
 /* form.onsubmit = function() {}
@@ -13,10 +12,25 @@ form.onsubmit = handleSubmit
 
 function handleSubmit(){} */
 
-form.onsubmit = function(event) {
-    event.preventDefault()
- 
-    const weight = inputWeight.value
-    const height = inputHeight.value
-    console.log(weight, height)
+const modalWrapper = document.querySelector(".modal-wrapper");
+const modalMessage = document.querySelector(".modal .title span");
+const modalBtnClose = document.querySelector(".modal button.close");
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const weight = inputWeight.value;
+  const height = inputHeight.value;
+
+  const result = IMC(weight, height);
+  const message = `Seu IMC é de ${result}`;
+
+  modalMessage.innerText = message;
+  modalWrapper.classList.add("open");
+};
+
+modalBtnClose.onclick = () => modalWrapper.classList.remove("open");
+
+function IMC(weight, height) {
+  return (weight / (height / 100) ** 2).toFixed(2);
 }
