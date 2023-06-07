eab409d88dd1e956128dce3f1ce387ac51982b32
Viviane Martini <martini.viviane@gmail.com>
2023-06-07 14:43:21 -0300

    add validation with error

--- stage5/projeto-imc-modules/js/script.js
+++ stage5/projeto-imc-modules/js/script.js
@@ -1,4 +1,5 @@
 import { Modal } from './modal.js';
+import { AlertError } from './alert-error.js'
 
 const form = document.querySelector("form");
 const inputWeight = document.querySelector("#weight");
@@ -13,9 +14,11 @@ form.onsubmit = (event) => {
  const showAlertError = notNumber(weight) || notNumber(height)
 
  if(showAlertError){
-  
+  AlertError.open()
   return;
  }
+
+ AlertError.close()
 
   const result = IMC(weight, height);
   const message = `Seu IMC é de ${result}`;
