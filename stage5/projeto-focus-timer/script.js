0398a9b9513a09683e51288e8651ad24b5cb8a61
Viviane Martini <martini.viviane@gmail.com>
2023-06-09 17:02:15 -0300

    add event-driven

--- stage5/projeto-focus-timer/script.js
+++ stage5/projeto-focus-timer/script.js
@@ -1,6 +1,17 @@
+// DOM
+// Document Object Model
 
 let play = document.querySelector('.play')
+let pause = document.querySelector('.pause')
+// Event-driven
+// programação imperativa
+// callback
+play.addEventListener('click', function() {
+  play.classList.add('hide')
+  pause.classList.remove('hide')
+})
 
-play.classList.add('hide')
-
-document.querySelector('.pause').classList.remove('hide')
+pause.addEventListener('click', function() {
+  pause.classList.add('hide')
+  play.classList.remove('hide')
+})
