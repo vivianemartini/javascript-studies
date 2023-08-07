3053c242b0c88673b067da3c8d75375828b5a7a6
Viviane Martini <martini.viviane@gmail.com>
2023-08-07 11:06:26 -0300

    activities on DOM also new focus-timer added

--- DOM/Elementos/script.js
+++ DOM/Elementos/script.js
@@ -1,10 +1,30 @@
+/* const animais = document.getElementById('animais')
+console.log(animais.innerText) */
+
 // Retorne no console todas as imagens do site
+
+const images = document.getElementsByTagName('img')
+//or querySelectorAll
 
 // Retorne no console apenas as imagens que começaram com a palavra imagem
 
+const imageAnimals = document.querySelectorAll('img[src^="./assets/image"]')
+
 // Selecione todos os links internos (onde o href começa com #)
+
+const linksInternos = document.querySelectorAll('[href^="#"]')
 
 // Selecione o primeiro h2 dentro de .animais-descricao
 
+const h2 = document.querySelector('.animais-descricao h2')
+
+
+//or
+
+const animais = document.querySelector('.animais-descricao')
+const h2Animais = animais.querySelector('h2')
+
 // Selecione o último p do site
+
+const p = document.querySelector('p:last-child')
 
